8bb45bfc4a31efa3cf96fe76c3f76bd05fa75632
Kelson Felipe Ribeiro Manozzo <kelson@FORLOGIC.LOCAL>
2018-12-19 08:11:27 -0200

    atividade sobre flexbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
+import { ContentComponent } from './layout/content/content.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
